4b6128c9c7b7b3c1ab0fb71440a11a0adf81e013
lightyears <lightyears1998@hotmail.com>
2023-03-16 19:42:27 +0800

    添加语料抽象

---
+++ App/WordBucket/Models/Corpus.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+
+namespace WordBucket.Models
+{
+    public class Corpus
+    {
+        [PrimaryKey]
+        public int? Id { set; get; }
+
+        public string Text { set; get; } = string.Empty;
+    }
+}
