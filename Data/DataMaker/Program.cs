6fa25f1b734700fcb3efe76c31aa1077a2f074df
lightyears <lightyears1998@hotmail.com>
2023-03-16 17:34:54 +0800

    召唤最强打工人

---
+++ Data/DataMaker/Program.cs
@@ -0,0 +1,13 @@
+﻿using WordBucket.Models;
+
+namespace WordBucket.DataMaker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var dictionary = new EnglishChineseDictionary(Id: 1, Name: "勇气");
+            Console.Write(dictionary);
+        }
+    }
+}
